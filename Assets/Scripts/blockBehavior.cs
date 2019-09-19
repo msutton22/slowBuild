b1f7ba038ec91fff6d68af25c5c5d80fd70f627b
Maddy <ms9642@nyu.edu>
2019-09-19 15:10:03 -0400

    Initial script made and basic sprites added

---
+++ Assets/Scripts/blockBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class blockBehavior : MonoBehaviour
+{
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.GetComponent<Transform> ().Translate (new Vector3 (speed, 0));			
+    }
+    
+    void OnCollisionEnter2D(Collision2D collision) //when you collide with enemy
+    {
+        if (collision.gameObject.tag.Equals("wall"))
+        {
+            speed = -speed;
+        }
+    }
+}
